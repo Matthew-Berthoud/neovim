dd7609c15b3302d3eefd5a6097fe187ae14866d1
Matthew Berthoud <matthewberthoud@gmail.com>
2025-04-07 14:35:25 -0400

    file tree for nvim

---
+++ lua/custom/plugins/nvim-tree.lua
@@ -0,0 +1,13 @@
+-- https://github.com/nvim-tree/nvim-tree.lua/wiki/Installation
+-- File tree plugin
+return {
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
+  lazy = false,
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    require('nvim-tree').setup {}
+  end,
+}
