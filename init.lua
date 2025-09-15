e9829239b9093b58404b76bbafafa629dbd11806
Matthew Berthoud <matthew.berthoud@blackcape.io>
2025-09-15 11:34:45 -0400

    ruff language server, not pyright

--- init.lua
+++ init.lua
@@ -679,7 +679,8 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         gopls = {},
-        pyright = {},
+        -- pyright = {},
+        ruff = {},
         phpactor = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
