7d5a6e0436d9f106632adb8dc1fd6f81e222c148
Matthew Berthoud <matthewberthoud@gmail.com>
2025-11-07 11:00:51 -0500

    Updated dotfiles.

--- init.lua
+++ init.lua
@@ -1024,5 +1024,76 @@ require('lazy').setup({
   },
 })
 
+local harpoon = require 'harpoon'
+
+local conf = require('telescope.config').values
+
+local function toggle_telescope(harpoon_files)
+  local file_paths = {}
+
+  for _, item in ipairs(harpoon_files.items) do
+    table.insert(file_paths, item.value)
+  end
+
+  local make_finder = function()
+    local paths = {}
+
+    for _, item in ipairs(harpoon_files.items) do
+      table.insert(paths, item.value)
+    end
+
+    return require('telescope.finders').new_table {
+      results = paths,
+    }
+  end
+
+  require('telescope.pickers')
+    .new({}, {
+      prompt_title = 'Harpoon',
+      finder = require('telescope.finders').new_table {
+        results = file_paths,
+      },
+      previewer = false,
+      sorter = conf.generic_sorter {},
+      layout_strategy = 'center',
+      layout_config = {
+        preview_cutoff = 1,
+        width = function(_, max_columns, _)
+          return math.min(max_columns, 80)
+        end,
+        height = function(_, _, max_lines)
+          return math.min(max_lines, 15)
+        end,
+      },
+      borderchars = {
+        prompt = { '─', '│', ' ', '│', '╭', '╮', '│', '│' },
+        results = { '─', '│', '─', '│', '├', '┤', '╯', '╰' },
+        preview = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+      },
+      attach_mappings = function(prompt_buffer_number, map)
+        -- The keymap you need
+        map('i', '<c-d>', function()
+          local state = require 'telescope.actions.state'
+          local selected_entry = state.get_selected_entry()
+          local current_picker = state.get_current_picker(prompt_buffer_number)
+
+          -- This is the line you need to remove the entry
+          harpoon:list():remove(selected_entry)
+          current_picker:refresh(make_finder())
+        end)
+
+        return true
+      end,
+    })
+    :find()
+end
+
+vim.keymap.set('n', '<C-e>', function()
+  toggle_telescope(harpoon:list())
+end, { desc = 'Open harpoon window' })
+vim.keymap.set('n', '<leader>a', function()
+  harpoon:list():add()
+end)
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
